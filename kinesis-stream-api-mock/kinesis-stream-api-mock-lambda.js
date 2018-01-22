444960c926b27e17acb6f2458963cd0ab94fbee0
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-22 16:24:24 +0530

    removed

--- kinesis-stream-api-mock/kinesis-stream-api-mock-lambda.js
+++ kinesis-stream-api-mock/kinesis-stream-api-mock-lambda.js
@@ -2,15 +2,5 @@ let AWS = require('aws-sdk');
 const kinesis = new AWS.Kinesis();
 exports.handler = function (event, context, callback) {
 
-	kinesis.describeStream({
-		StreamName: 'sample-kinesis-stream',
-		ExclusiveStartShardId: 'shardId-000000000000'
-	}).promise()
-		.then(describeStreamData => {
-			callback(null, describeStreamData);
-		})
-		.catch(err => {
-			callback(err, 'Error in executing Kinesis#describeStream');
-		});
 
 }
