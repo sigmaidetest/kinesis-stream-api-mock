a9c41cbb566c263908196854ab4d9b7b15dde8f2
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-22 19:30:21 +0530

    Added Kinesis#describeStream

--- kinesis-stream-api-mock/kinesis-stream-api-mock-lambda.js
+++ kinesis-stream-api-mock/kinesis-stream-api-mock-lambda.js
@@ -2,5 +2,14 @@ let AWS = require('aws-sdk');
 const kinesis = new AWS.Kinesis();
 exports.handler = function (event, context, callback) {
 
-
+    kinesis.describeStream({
+        StreamName: 'kinesis-sample-stream'
+    }).promise()
+        .then(describeStreamData => {
+            callback(null, describeStreamData);
+        })
+        .catch(err => {
+            callback(err, 'Error in executing Kinesis#describeStream');
+        });
+        
 }
