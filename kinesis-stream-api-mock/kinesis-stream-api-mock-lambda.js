8de139e0500addb217f8be6cf3c734bce64ae649
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-22 14:16:17 +0530

    Added exclusive shard ID to describe stream

--- kinesis-stream-api-mock/kinesis-stream-api-mock-lambda.js
+++ kinesis-stream-api-mock/kinesis-stream-api-mock-lambda.js
@@ -3,7 +3,8 @@ const kinesis = new AWS.Kinesis();
 exports.handler = function (event, context, callback) {
 
 	kinesis.describeStream({
-		StreamName: 'sample-kinesis-stream'
+		StreamName: 'sample-kinesis-stream',
+		ExclusiveStartShardId: 'shardId-000000000000'
 	}).promise()
 		.then(describeStreamData => {
 			callback(null, describeStreamData);
