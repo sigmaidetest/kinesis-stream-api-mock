55dbd05761ffa1ffdf92eba1222a325265b3665f
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-22 13:58:58 +0530

    Added Kinesis resource & APIG trigger

---
+++ kinesis-stream-api-mock/kinesis-stream-api-mock-lambda.js
@@ -0,0 +1,20 @@
+let AWS = require('aws-sdk');
+const kinesis = new AWS.Kinesis();
+exports.handler = function (event, context, callback) {
+
+	kinesis.describeStream({
+		StreamName: 'sample-kinesis-stream'
+	}).promise()
+		.then(describeStreamData => {
+			callback(null, {
+				'statusCode': 200,
+				'body': JSON.stringify('Stream', describeStreamData),
+				'isBase64Encoded': false
+			});
+		})
+		.catch(err => {
+			callback(err, 'Error in executing Kinesis#describeStream');
+		});
+
+	callback(null, 'Successfully executed');
+}
